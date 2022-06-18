c53356131a417c903d4e9a349f0928df3f39f9cf
antonelacortes <103968770+antonelacortes@users.noreply.github.com>
2022-06-18 14:10:15 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function cambiarfondo1() {
+    document.body.style.backgroundColor = "#1b2f33";
+    document.getElementById("header").style.color = "#f7ebe8";
+}
+function cambiarfondo2() {document.body.style.backgroundColor = "#f7ebe8";}
+
+
+function copy() {
+    alert("Autora: Antonela Cortes. E-mail: ux.antonelacortes@gmail.com");
+    }
