82f58708c1d3b203cbff9bd1de5d1d6d500df68a
antonelacortes <103968770+antonelacortes@users.noreply.github.com>
2022-06-19 11:16:20 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,31 @@
 function cambiarfondo1() {
     document.body.style.backgroundColor = "#1b2f33";
-    document.getElementById("header").style.color = "#f7ebe8";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("nav").style.backgroundColor = "#1b2f33";
+    document.getElementById("nav").style.color = "white";
+    document.getElementById("section").style.backgroundColor = "#1b2f33";
+    document.getElementById("section").style.color = "white";
+    document.getElementById("aside").style.backgroundColor = "#1b2f33";
+    document.getElementById("aside").style.color = "#1b2f33";
+    document.getElementById("div").style.backgroundColor = "#1b2f33";
+    document.getElementById("div").style.color = "white";
+    document.getElementById("footer").style.backgroundColor = "#1b2f33";
+    document.getElementById("footer").style.color = "white";
 }
-function cambiarfondo2() {document.body.style.backgroundColor = "#f7ebe8";}
+function cambiarfondo2() {document.body.style.backgroundColor = "#f7ebe8";
+    document.getElementById("header").style.color = "#1b2f33";
+    document.getElementById("header").style.backgroundColor = "#f7ebe8";
+    document.getElementById("nav").style.color = "##1b2f33";
+    document.getElementById("nav").style.backgroundColor = "#f7ebe8";
+    document.getElementById("section").style.color = "#1b2f33";
+    document.getElementById("section").style.backgroundColor = "#f7ebe8";
+    document.getElementById("aside").style.color = "#1b2f33";
+    document.getElementById("aside").style.backgroundColor = "#f7ebe8";
+    document.getElementById("div").style.color = "#1b2f33";
+    document.getElementById("div").style.backgroundColor = "#f7ebe8";
+    document.getElementById("footer").style.color = "#1b2f33";
+    document.getElementById("footer").style.backgroundColor = "#f7ebe8";
+}
 
 
 function copy() {
